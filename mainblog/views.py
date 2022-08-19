5e069669dcf0cd69d6eceb11ac9c8ccb16dad45c
matt70iu <mattadams60@gmail.com>
2022-08-19 18:15:05 +0000

    create test home func, add to views and admin, migrate and test

--- mainblog/views.py
+++ mainblog/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to code zone!</h1>')
+    context = {}
+    return render(request, 'home.html', context )
