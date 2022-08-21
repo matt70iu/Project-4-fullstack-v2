5c7e4dbd379b7bbf38ad1559fcda567fe6c53a3e
matt70iu <mattadams60@gmail.com>
2022-08-21 01:24:11 +0000

    remove title tag from db for now and migrate

--- mainblog/models.py
+++ mainblog/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default="Welcome to Code Zone")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
