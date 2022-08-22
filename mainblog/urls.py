d238775855ae2aed90b72f32a8e61946e0a447e7
matt70iu <mattadams60@gmail.com>
2022-08-22 13:15:45 +0000

    add edit post page, update templates, urls and views to suit

--- mainblog/urls.py
+++ mainblog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, ArticleDetailView, NewPostView
+from .views import HomeView, ArticleDetailView, NewPostView, EditPostView
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('new_post/', NewPostView.as_view(), name='new_post'),
+    path('article/edit/<int:pk>', EditPostView.as_view(), name='update_post'),
 
 
 ]
--- mainblog/views.py
+++ mainblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 
@@ -23,4 +23,10 @@ class NewPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'new_post.html'
-    #fields = '__all__'
+    # fields = '__all__'
+
+
+class EditPostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'title_tag', 'body']
