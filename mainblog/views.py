4eeb59116b687ed6355c020b64604b9d2b4889fc
matt70iu <mattadams60@gmail.com>
2022-08-23 22:29:53 +0000

    add logic to views to dynamically add Catagories, added dopdown to base with links to base and tested

--- mainblog/views.py
+++ mainblog/views.py
@@ -15,6 +15,12 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-post_date', '-post_time']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats)
