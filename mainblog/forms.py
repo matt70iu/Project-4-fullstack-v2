18388eb1c14a4d67d049fb0a03e4aa699802fcc7
matt70iu <mattadams60@gmail.com>
2022-08-24 14:21:12 +0000

    add 2nd name author to home, add js logic to newpost to ensure correct author can create post and test

--- mainblog/forms.py
+++ mainblog/forms.py
@@ -18,7 +18,9 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control',
+                                             'value': '', 'id': 'author-id',
+                                             'type': 'hidden'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
