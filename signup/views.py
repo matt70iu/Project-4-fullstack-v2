e2ca19f209462ccec0dec368ee6a207c8bc43a33
matt70iu <mattadams60@gmail.com>
2022-08-24 17:32:39 +0000

    create reg form to replace default, ammend logic in views and test

--- signup/views.py
+++ signup/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegistrationForm
 
 
 class UserCreateView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegistrationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
