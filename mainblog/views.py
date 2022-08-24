d762b40e41122ede2b1945d1c06d5d9e44c92ab6
matt70iu <mattadams60@gmail.com>
2022-08-24 16:38:47 +0000

    add loginc for unlike button, added to article template and test

--- mainblog/views.py
+++ mainblog/views.py
@@ -14,7 +14,14 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -45,8 +52,13 @@ class ArticleDetailView(DetailView):
             *args, **kwargs)
         items = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = items.total_likes()
+
+        liked = False
+        if items.likes.filter(id=self.request.user.id).exists():
+            liked = True
         context["cat_menu"] = cat_menu
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 
