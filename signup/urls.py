7e7c6540ee491300ebb3872d38fa55580b876e03
matt70iu <mattadams60@gmail.com>
2022-08-25 11:53:20 +0000

    create prov edit profile page, write logic to pull current profile and test, add to Base, ursl and views

--- signup/urls.py
+++ signup/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserCreateView
+from .views import UserCreateView, UserEdiView
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='create_user'),
+    path('edit_profile/', UserEdiView.as_view(), name='edit_profile'),
 
 
 
--- signup/views.py
+++ signup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import RegistrationForm
 
@@ -8,3 +9,12 @@ class UserCreateView(generic.CreateView):
     form_class = RegistrationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEdiView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
