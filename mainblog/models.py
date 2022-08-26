690b265c8cccb9e6de87b0d84e2a459353381f6d
matt70iu <mattadams60@gmail.com>
2022-08-26 22:53:52 +0000

    add logic for profile pic, and ammend admin, tamplates and migrate

--- mainblog/models.py
+++ mainblog/models.py
@@ -15,6 +15,20 @@ class Category(models.Model):
         return reverse('home')
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_picture = models.ImageField(
+        null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     header_image = models.ImageField(
