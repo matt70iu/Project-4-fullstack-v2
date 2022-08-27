61bc6f370766e1727552bfb99d5df9263317c9d2
matt70iu <mattadams60@gmail.com>
2022-08-27 13:26:05 +0000

    add logic for profile pic, and social med links, links not yet functional, migrate changes and test

--- mainblog/models.py
+++ mainblog/models.py
@@ -18,7 +18,12 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
-
+    profile_picture = models.ImageField(
+        null=True, blank=True, upload_to='media/images/profile/')
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
