bab57241224fd99c41a66edf1cf7c3eb6d0500aa
matt70iu <mattadams60@gmail.com>
2022-08-29 18:33:55 +0000

    add link to profile to article detail, add url and view logic for main profile page

--- signup/urls.py
+++ signup/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from .views import UserCreateView, UserEdiView, PasswordsChangeView
+from .views import UserCreateView, UserEdiView, PasswordsChangeView, MainProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='create_user'),
     path('edit_profile/', UserEdiView.as_view(), name='edit_profile'),
-    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
     path('password/', PasswordsChangeView.as_view(
         template_name='registration/change-password.html')),
     path(' password_success', views.password_success, name="password_success"),
+    path('<int:pk>/profile/', MainProfilePageView.as_view(),
+         name='show_profile_page'),
+
 
 
 
