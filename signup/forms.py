3f41968e49d7c3a71743211c86955f03b7809819
matt70iu <mattadams60@gmail.com>
2022-08-30 20:16:32 +0000

    create custom user profile form, ammend models and views as needed to render and test

--- signup/forms.py
+++ signup/forms.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
+from mainblog.models import Profile
+
+
+class ProfilePageForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'profile_picture', 'website_url',
+                'facebook_url', 'instagram_url', 'twitter_url')
+
+        widgets = {
+            'bio': forms.Textarea(attrs={'class': 'form-control'}),
+            'website_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'facebook_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'instagram_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'twitter_url': forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
 
 
 class RegistrationForm(UserCreationForm):
--- signup/views.py
+++ signup/views.py
@@ -4,14 +4,14 @@ from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import RegistrationForm, EditProfileForm, PasswordChangedForm
+from .forms import RegistrationForm, EditProfileForm, PasswordChangedForm, ProfilePageForm
 from mainblog.models import Profile
 
 
 class CreateProfilePageView(CreateView):
     model = Profile
+    form_class = ProfilePageForm
     template_name = "registration/create_user_profile_page.html"
-    fields = '__all__'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
