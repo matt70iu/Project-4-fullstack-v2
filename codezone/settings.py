11258f1a460773c87d75850cc26b5c2ba6c1f5bf
matt70iu <mattadams60@gmail.com>
2022-09-01 12:27:39 +0000

    profile details under logged in user and remove previous profile menu, add x-frames to settings for Heroku

--- codezone/settings.py
+++ codezone/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['codezone-p4-v2.herokuapp.com', 'localhost']
 
 
