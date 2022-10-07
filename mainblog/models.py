228e994fe0619f6404b22493777e0259921b63e4
matt70iu <mattadams60@gmail.com>
2022-10-07 14:18:25 +0000

    refactor model code to remove pylint error

--- mainblog/models.py
+++ mainblog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
+# pylint: disable=no-member
 
 
 class Category(models.Model):
@@ -11,7 +12,7 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def get_absolute_url(self):
         '''Redirect back to home'''
