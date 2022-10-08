bb9475dcaeb6d3acd8b3855ed87a68fe8e75ebda
matt70iu <mattadams60@gmail.com>
2022-10-08 14:23:50 +0000

    remove defunct ck editor import from models, ammend article detail and profile pages so all profile links open new tab

--- mainblog/models.py
+++ mainblog/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ckeditor.fields import RichTextField
 # pylint: disable=no-member
 
 
